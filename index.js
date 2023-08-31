5a7d296707ef201c572faf123cb75399f0ea8928
ichiken26 <97601028+ichiken26@users.noreply.github.com>
2023-09-01 04:51:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ document.addEventListener("DOMContentLoaded", function() {
     nowtimeSearch.addEventListener("click", function(event) {
         const clickednowtimeSearch = event.target.textContent;
         if (clickednowtimeSearch){
-            window.location.href = "https://ichiken26.github.io/search_class/nowtime/faculties.html";
+            window.location.href = "https://ichiken26.github.io/search_class/nowtime/faculties";
         }
     });
 
     choicetimeSearch.addEventListener("click", function(event) {
         const clickedchoicetimeSearch = event.target.textContent;
         if (clickedchoicetimeSearch){
-            window.location.href = "https://ichiken26.github.io/search_class/choicetime/choicetime.html";
+            window.location.href = "https://ichiken26.github.io/search_class/choicetime/choicetime";
         }
     });
 
     classroomsSearch.addEventListener("click", function(event) {
         const clickedclassroomsSearch = event.target.textContent;
         if (clickedclassroomsSearch){
-            window.location.href = "https://ichiken26.github.io/search_class/classrooms/classrooms.html";
+            window.location.href = "https://ichiken26.github.io/search_class/classrooms/classrooms";
         }
     });
 });
